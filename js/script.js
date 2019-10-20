914a0041bf94fc8442c81ae0ba8651c51a87a67d
isabellawhitfield <270026136@bun2-206-08.acg.ac.nz>
2019-10-21 12:39:12 +1300

    photos

---
+++ js/script.js
@@ -0,0 +1,59 @@
+console.log('Array of objects');
+
+var dogs = [
+ {
+     name : "Stacey Lane Mathews",
+     id : "ch001",
+     breed : "Chiwhawha",
+     color : "White and tan",
+     height: 20,
+     age : 7,
+     photos: "images/chiwhawha.jpeg"
+
+
+ },
+ {
+    name : "Susan Brittany",
+    id : "fr001",
+    breed : "French Bulldog",
+    color : "Black",
+    height: 30,
+    age : 4,
+    photos: "images/frenchBull.jpg"
+
+
+},
+{
+    name : "Vincent Peters",
+    id : "hu001",
+    breed : "Husky",
+    color : "White and grey",
+    height: 60,
+    age : 2,
+    photos: "images/husky.jpeg"
+
+
+},
+{
+    name : "Friedrich Heinz",
+    id : "ge001",
+    breed : "German Sheppard",
+    color : "Black and brown",
+    height: 50,
+    age : 9,
+    photos: "images/germanSheppard.jpg"
+
+
+},
+{
+    name : "George Thomas",
+    id : "pu001",
+    breed : "Pug",
+    color : "Black and tan",
+    height: 15,
+    age : 5,
+    photos: "images/pug.jpg"
+
+
+},
+];
